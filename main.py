b1c8ad1991f87115cea654e5a4569e828720db6a
jeru27 <jrumora@gmail.com>
2021-01-28 15:43:33 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+secret = 2
+
+guess = int(input("Guess the secret number from 1-10: "))
+
+if guess == secret:
+    print("Congratulations! The secret number is 2!")
+else:
+    print("The answer is wrong.")
